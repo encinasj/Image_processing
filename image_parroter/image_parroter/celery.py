9a100908c7ac4d1ad967a7f6771184c492dd49ce
encinasj <encinasj.angel@gmail.com>
2022-02-06 05:53:41 -0700

    creating a simple celery task

--- image_parroter/image_parroter/celery.py
+++ image_parroter/image_parroter/celery.py
@@ -1,8 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGs_MODULE', 'image_parroter.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_parroter.settings')
 
 celery_app = Celery('image_parroter')
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
